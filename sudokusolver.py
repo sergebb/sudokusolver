8e708decdc48c24344960a129717de3047c53bd5
Sergey Bobkov <thereisalogin@gmail.com>
2017-09-14 11:22:51 +0300

    Print puzzle in nice way; Recursive solver; code style cleanup

--- sudokusolver.py
+++ sudokusolver.py
@@ -2,22 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import sys
 import os
 import numpy as np
 
-class Solver:
+class Solver(object):
     table = None
     possible_table = None
-    def __init__(self):
-        self.table = None
-        self.possible_table = None
+    verbose = False
 
-    def __init__(self, input_problem):
+    def __init__(self, input_problem, verbose=False):
+        self.verbose = verbose
         self.load_problem(input_problem)
 
     def load_problem(self, input_problem):
-        if type(input_problem) is np.ndarray and input_problem.ndim == 2 and \
+        if isinstance(input_problem, np.ndarray) and input_problem.ndim == 2 and \
                 input_problem.shape[0] == 9 and input_problem.shape[1] == 9:
             self.table = input_problem
             self.fill_possible_table()
@@ -25,7 +23,7 @@ class Solver:
     def get_result(self):
         return self.table
 
-    def calculate(self):
+    def start(self):
         while True:
             y, x, val = self.find_cell_with_one_possibility()
 
@@ -39,64 +37,98 @@ class Solver:
                 y, x, val = self.find_one_possible_in_square()
 
             if y < 0:
-                #No other results
+                # No other results
                 break
 
             # We found something
-            print 'X,Y = %d,%d <- %d' % (x+1, y+1, val)
+            if self.verbose:
+                print 'X,Y = %d,%d <- %d' % (x+1, y+1, val)
             self.table[y, x] = val
             self.remove_possibility(y, x, val)
 
+        if not self.is_solved():
+            #Recursive test of possible values:
+            for num_possibilities in range(2, 9):
+                y, x, vals = self.find_cell_with_n_possibilities(num_possibilities)
+
+                if y >= 0:
+                    for val in vals:
+                        solved_table = self.recursive_test_cell_possibility(y, x, val)
+                        if solved_table is not None:
+                            self.table = solved_table
+                            break
+
+    def is_solved(self):
+        for i in range(9):
+            if self.table[i, :].sum() != 45:
+                return False
+            if self.table[:, i].sum() != 45:
+                return False
+            sq_y = i//3
+            sq_x = i%3
+            if self.square(sq_y*3, sq_x*3).sum() != 45:
+                return False
+        # All test passed
+        return True
+
     def find_cell_with_one_possibility(self):
-        y_list, x_list = np.where(np.sum(self.possible_table, axis=2) == 1)
-        if len(y_list) > 0:
-            y = y_list[0]
-            x = x_list[0]
+        res = np.where(np.sum(self.possible_table, axis=2) == 1)
+        if len(res[0]) > 0:
+            y = res[0][0]
+            x = res[1][0]
             val_idx = np.where(self.possible_table[y, x] == 1)[0][0]
             return y, x, val_idx+1
-        else:
-            return -1, -1, -1
+        return -1, -1, -1
+
+    def find_cell_with_n_possibilities(self, num_possibilities):
+        res = np.where(np.sum(self.possible_table, axis=2) == num_possibilities)
+        if len(res[0]) > 0:
+            y = res[0][0]
+            x = res[1][0]
+            val_idxs = np.where(self.possible_table[y, x] == 1)[0]
+            return y, x, val_idxs+1
+        return -1, -1, -1
 
     def find_one_possible_in_col(self):
-        y_list, val_list = np.where(np.sum(self.possible_table, axis=1) == 1)
-        if len(y_list) > 0:
-            y = y_list[0]
-            val_idx = val_list[0]
-            x = np.where(self.possible_table[y,:,val_idx] == 1)[0][0]
+        res = np.where(np.sum(self.possible_table, axis=1) == 1)
+        if len(res[0]) > 0:
+            y = res[0][0]
+            val_idx = res[1][0]
+            x = np.where(self.possible_table[y, :, val_idx] == 1)[0][0]
             return y, x, val_idx+1
-        else:
-            return -1, -1, -1
+        return -1, -1, -1
 
     def find_one_possible_in_row(self):
-        x_list, val_list = np.where(np.sum(self.possible_table, axis=0) == 1)
-        if len(x_list) > 0:
-            x = x_list[0]
-            val_idx = val_list[0]
-            y = np.where(self.possible_table[:,x,val_idx] == 1)[0][0]
+        res = np.where(np.sum(self.possible_table, axis=0) == 1)
+        if len(res[0]) > 0:
+            x = res[0][0]
+            val_idx = res[1][0]
+            y = np.where(self.possible_table[:, x, val_idx] == 1)[0][0]
             return y, x, val_idx+1
-        else:
-            return -1, -1, -1
+        return -1, -1, -1
 
     def find_one_possible_in_square(self):
-        res = np.where(self.possible_table.reshape(3,3,3,3,9).sum(axis = (1,3)) == 1)
+        res = np.where(self.possible_table.reshape(3, 3, 3, 3, 9).sum(axis = (1, 3)) == 1)
         if len(res[0]) > 0:
             sq_y = res[0][0]
             sq_x = res[1][0]
             val_idx = res[2][0]
-            relative_res = np.where(self.possible_square(sq_y*3, sq_x*3)[:,:,val_idx] == 1)
+            relative_res = np.where(self.possible_square(sq_y*3, sq_x*3)[:, :, val_idx] == 1)
             y = sq_y*3 + relative_res[0][0]
             x = sq_x*3 + relative_res[1][0]
             return y, x, val_idx+1
+        return -1, -1, -1
 
+    def recursive_test_cell_possibility(self, y, x, val):
+        new_table = self.table.copy()
+        new_table[y, x] = val
+        new_solver = Solver(new_table)
+        new_solver.start()
+        if new_solver.is_solved():
+            return new_solver.get_result()
+        #We found nothing
+        return None
 
-        # x_list, val_list = np.where(np.sum(self.possible_table, axis=0) == 1)
-        # if len(x_list) > 0:
-        #     x = x_list[0]
-        #     val_idx = val_list[0]
-        #     y = np.where(self.possible_table[:,x,val_idx] == 1)[0][0]
-        #     return y, x, val_idx+1
-        else:
-            return -1, -1, -1
 
     def remove_possibility(self, y, x, value):
         self.possible_table[y, x, :] = 0 # Block filled cell
@@ -109,16 +141,17 @@ class Solver:
     def possible_square(self, y, x):
         sq_x = x//3
         sq_y = y//3
-        return self.possible_table.reshape(3,3,3,3,9)[sq_y,:,sq_x,:,:]
+        return self.possible_table.reshape(3, 3, 3, 3, 9)[sq_y, :, sq_x, :, :]
 
     def fill_possible_table(self):
-        self.possible_table = np.zeros(9*9*9, dtype=np.uint).reshape(9, 9, 9)   # 3rd axis for vals, 0-8 index for 1-9 vals, so val=idx+1
+        # 3rd axis for vals, 0-8 index for 1-9 vals, so val=idx+1
+        self.possible_table = np.zeros(9*9*9, dtype=np.uint8).reshape(9, 9, 9)   
         for x in range(9):
             for y in range(9):
                 if self.table[y, x] == 0:
                     possible_vals = self.get_possible_vals(y, x)
                     for val in possible_vals:
-                        self.possible_table[y, x, val-1] = 1 
+                        self.possible_table[y, x, val-1] = 1
 
 
     def get_possible_vals(self, y, x):
@@ -129,10 +162,10 @@ class Solver:
 
         forbidden_vals = set(crossed_cells)
 
-        if 0 in forbidden_vals: 
+        if 0 in forbidden_vals:
             forbidden_vals.remove(0)
 
-        possible_vals = [val for val in range(1,10) if val not in forbidden_vals]
+        possible_vals = [val for val in range(1, 10) if val not in forbidden_vals]
         return possible_vals
 
     def row(self, y):
@@ -144,14 +177,7 @@ class Solver:
     def square(self, y, x):
         sq_x = x//3
         sq_y = y//3
-        return self.table.reshape(3,3,3,3)[sq_y,:,sq_x,:]
-
-    def test_solver(self):
-        print 'Table:\n', self.table
-        print 'Squares:\n', self.square(0,0), self.square(0,3), self.square(0,6)
-        print self.square(3,0), self.square(3,3), self.square(4,6)
-        print self.square(6,0), self.square(7,3), self.square(8,6)
-        print 'Possible vals:\n', np.sum(self.possible_table, axis=2)
+        return self.table.reshape(3, 3, 3, 3)[sq_y, :, sq_x, :]
 
 
 def parse_sudoku_file(sudoku_file):
@@ -167,7 +193,21 @@ def parse_sudoku_file(sudoku_file):
     if len(array) != 9:
         return None
 
-    return np.array(array, dtype=np.uint32)
+    return np.array(array, dtype=np.uint8)
+
+def print_puzzle(table):
+    for y in range(9):
+        if y == 3 or y == 6:
+            print '------+-------+-------'
+        for x in range(9):
+            if x == 3 or x == 6:
+                print '|',
+            val = table[y, x]
+            if val == 0:
+                print '.',
+            else:
+                print val,
+        print ''
 
 
 def main():
@@ -190,9 +230,8 @@ def main():
     solver = Solver(input_problem)
 
     # solver.test_solver()
-    print input_problem
-    solver.calculate()
-    print solver.get_result()
+    solver.start()
+    print_puzzle(solver.get_result())
 
 if __name__ == '__main__':
     main()
